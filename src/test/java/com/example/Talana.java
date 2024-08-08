936d02b8ef75914b81bef297c084efdbbbbbf6d9
mcastro <mcastro@talana.com>
2024-08-08 16:47:58 -0500

    Se agrega mostrar x consola resultados

--- src/test/java/com/example/Talana.java
+++ src/test/java/com/example/Talana.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -14,7 +15,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class Talana {
     private AndroidDriver<MobileElement> driver;
@@ -34,23 +37,46 @@ public class Talana {
 
     @Test
     public void testTalanaApp() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
 
-     //Acá es donde empiezo a crear los metodos para las pruebas  
+        // Esperar que el Spinner esté presente y sea clickable
+        WebElement el2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.Spinner[@resource-id='com.talana.nextqa:id/enviroment_ambient']")));
+        el2.click(); // Hacer clic en el Spinner para mostrar la lista
 
-   
-    WebDriverWait wait = new WebDriverWait(driver, 30);  // Esperar que el elemento esté presente y sea clickable
-    WebElement el1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.view.ViewGroup"))); // Encontrar el elemento por xpath
-    
-    el1.click(); // Hacer clic en el elemento
+        // Esperar unos segundos para asegurarnos de que la lista se haya desplegado
+        try {
+            Thread.sleep(2000); // Espera de 2 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    WebElement el2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.Spinner[@resource-id='com.talana.nextqa:id/enviroment_ambient']")));
-    el2.click(); // Hacer clic en el segundo elemento, listbox
+        // Imprimir todos los elementos TextView visibles
+        List<MobileElement> options = driver.findElements(By.className("android.widget.TextView"));
+        System.out.println("Elementos TextView disponibles:");
+        for (MobileElement option : options) {
+            System.out.println(option.getText());
+        }
 
-    WebElement migrationOption = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.Spinner[@resource-id=\"com.talana.nextqa:id/enviroment_ambient\"]")));
-    migrationOption.click();   
+        // Esperar que la opción "Migración PE" esté presente y sea clickable
+        WebElement migrationOption = null;
+        for (MobileElement option : options) {
+            if (option.getText().contains("migracion pe")) {
+                migrationOption = option;
+                break;
+            }
+        }
 
-}
+        if (migrationOption != null) {
+            System.out.println("Texto del elemento encontrado: " + migrationOption.getText());
+            System.out.println("Estado del elemento: Enabled=" + migrationOption.isEnabled() + ", Displayed=" + migrationOption.isDisplayed());
 
+            // Usar TouchAction para hacer clic en las coordenadas del elemento
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.tap(PointOption.point(migrationOption.getLocation().getX(), migrationOption.getLocation().getY())).perform();
+        } else {
+            System.out.println("Elemento 'migracion pe' no encontrado.");
+        }
+    }
 
     @After
     public void tearDown() {
